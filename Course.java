0170265ef80a2ec8eefb4f5bbc366245af59042c
Rimmer7 <crispin91@hotmail.com>
2015-06-02 12:34:04 +0200

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,1 +1,30 @@
-
+public class Course() {
+	private ArrayList<Schedule> labGroups;
+	private ArrayList<Schedule> tutors;
+	
+	public Course() {
+		labGroups = new ArrayList<Schedule>();
+		tutors = new ArrayList<Schedule();
+	}
+	
+	public void addLab(Schedule s) {
+		labGroups.add(s);
+	}
+	
+	public void addTutor(Schedule s) {
+		tutors.add(s);
+	}
+	
+	public void printCandidates() {
+		StringBuilder sb = new StringBuilder();
+		for(group : labGroups) {
+			sb.append(group);
+			for(tutor : tutors) {
+				if(group.containsAll(tutor)) {
+					sb.append("\n" + tutor.getName());
+				}
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
