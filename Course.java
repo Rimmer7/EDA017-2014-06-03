afe52c4362abcdaadd06d7890e3da4d840d45aaf
Rimmer7 <crispin91@hotmail.com>
2015-06-02 12:39:16 +0200

    Update Course.java

--- Course.java
+++ Course.java
@@ -2,19 +2,24 @@ public class Course() {
 	private ArrayList<Schedule> labGroups;
 	private ArrayList<Schedule> tutors;
 	
+	/** Skapar ett Course-objekt utan några labbgrupps- eller handledartider. */
 	public Course() {
 		labGroups = new ArrayList<Schedule>();
 		tutors = new ArrayList<Schedule();
 	}
 	
+	/** Lägger till en labbgrupps tider. */
 	public void addLab(Schedule s) {
 		labGroups.add(s);
 	}
 	
+	/** Lägger till en handledares möjliga arbetstider. */
 	public void addTutor(Schedule s) {
 		tutors.add(s);
 	}
 	
+	/** Skriver ut alla labbgrupper. För varje labbgrupp skrivs namnen på de
+	handledare som har möjlighet att handleda gruppen ut. */
 	public void printCandidates() {
 		StringBuilder sb = new StringBuilder();
 		for(group : labGroups) {
